295b68551e47127b06fd01bd1e219a57fdf3fe00
SyedAleezHassan <syedaleezhassan@gmal.com>
2024-05-01 05:03:01 +0500

    new work about Api get

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
+import 'package:covid_track_app/model.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,36 +12,70 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<SamplePosts> samplePosts = [];
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 1,
-      itemBuilder: (context,index) {
-        return Container(
-          height: 130,
-          color: Colors.grey,
-          padding: const EdgeInsets.all(3),
-          margin: const  EdgeInsets.all(5),
-          child: const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('user Id :data',
-              style: TextStyle(fontSize: 18),
-              ),
-                          Text('Id :data',
-              style: TextStyle(fontSize: 18),
-              ),
-                          Text('Title :data',
-              style: TextStyle(fontSize: 18),
-              ),
-                          Text('Body :data',
-              style: TextStyle(fontSize: 18),
-              ),
-            ],
-          ),
-        );
-      }
+    return FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: 1,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 130,
+                    color: Colors.grey,
+                    padding: const EdgeInsets.all(3),
+                    margin: const EdgeInsets.all(5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'user Id :${samplePosts[index].userId}',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Text(
+                          'Id :${samplePosts[index].id}',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Text(
+                          'Title :${samplePosts[index].title}',
+                          style: TextStyle(fontSize: 18),
+                          maxLines: 1,
+                        ),
+                        Text(
+                          'Body :${samplePosts[index].body} ',
+                          style: TextStyle(fontSize: 18),
+                          maxLines: 1,
+                        ),
+                      ],
+                    ),
+                  );
+                });
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
+  }
+
+  Future<List<SamplePosts>> getData() async {
+    final response = await http.get(
+      Uri.parse(
+        'https://jsonplaceholder.typicode.com/posts',
+      ),
     );
+    var data = jsonDecode(response.body.toString());
+
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> index in data) {
+        samplePosts.add(SamplePosts.fromJson(index));
+      }
+      return samplePosts;
+    } else {
+      return samplePosts;
+    }
   }
 }
